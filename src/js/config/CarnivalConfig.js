2eae2be970eb0f9df2bc40ffc7de405adb6690c3
Joao Kiyoshi <joao@vizir.com.br>
2014-11-24 14:52:02 -0200

    Returning provider to one file

--- src/js/config/CarnivalConfig.js
+++ src/js/config/CarnivalConfig.js
@@ -1,10 +1,32 @@
 define(function (require) {
 
   var angular = require('angular');
-
   var config = angular.module('config', []);
 
-  config.provider('CarnivalConfig', require('config/ApiConfig.js'));
+  config.provider('CarnivalConfig', function () {
+    
+    var baseApiUrl = null;
+
+    return {
+
+      setBaseApiUrl: function (url) {
+        url.replace(/\/$/, '');
+        baseApiUrl = url;
+      },
+
+      $get: function () {
+        return {
+
+          getBaseApiUrl: function () {
+            return baseApiUrl;
+          }
+
+        };
+      }
+
+    };
+
+  });
 
   return config;
 
