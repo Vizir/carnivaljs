e58c5a88fb316c6f224c7448fda109e09c051bd4
Joao Kiyoshi <joao@vizir.com>
2014-12-08 16:20:21 -0200

    Remove edit-button from components dependencies

--- app/components/components.js
+++ app/components/components.js
@@ -3,7 +3,6 @@ angular.module('carnival.components', [
   'carnival.components.belongstofield',
   'carnival.components.form',
   'carnival.components.delete-button',
-  'carnival.components.edit-button',
   'carnival.components.hasmanyfield',
   'carnival.components.listing',
   'carnival.components.navbar',
