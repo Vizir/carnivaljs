ec21c1cace1dcdbba2678973ab4885b278bcf747
Joao Kiyoshi <joao@vizir.com>
2014-12-15 10:40:36 -0200

    Remove useApplyAsync on  service

--- app/carnival.js
+++ app/carnival.js
@@ -5,7 +5,7 @@ angular.module('carnival', [
   'gettext',
   'angular-loading-bar'
 ])
-.config(function ($stateProvider, $urlRouterProvider, $httpProvider) {
+.config(function ($stateProvider, $urlRouterProvider) {
 
   $urlRouterProvider.otherwise('/');
   $stateProvider
@@ -34,8 +34,6 @@ angular.module('carnival', [
       templateUrl: 'states/main.edit/edit.html',
       controller: 'EditController'
     });
-
-    $httpProvider.useApplyAsync(true);
 
 })
 .run(function (gettextCatalog, Configuration, Entity){
