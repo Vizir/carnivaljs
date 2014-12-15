00f31d77453e6cf6415d7788e2e54017b2f8cbc8
diegonakamashi <diegonakamashi@gmail.com>
2014-12-15 16:38:39 -0200

    [carnival-listing-field] - Adição de testes

--- app/components/listing-field-belongs-to/listing-field-belongs-to.js
+++ app/components/listing-field-belongs-to/listing-field-belongs-to.js
@@ -12,16 +12,16 @@ angular.module('carnival.components.listingfieldbelongsto', [])
       var entityModel = Configuration.getEntity($stateParams.entity);
         
       $scope.getUrl = function(){
-        var fieldUrl = $scope.item[$scope.field.name + 'Url']
+        var fieldUrl = $scope.item[$scope.field.name + 'Url'];
         if(!fieldUrl)
-          fieldUrl = '#/show/' + $scope.field.resourceName + '/' + $scope.item[$scope.field.name].id
+          fieldUrl = '#/show/' + $scope.field.resourceName + '/' + $scope.item[$scope.field.name].id;
         return fieldUrl;
-      }
+      };
 
       $scope.getLabel = function(){
         var indexProperties = $scope.field.views.index;
         return $scope.item[$scope.field.name][indexProperties.label];
-      }
+      };
     }
   };
 });
--- app/components/listing-field/listing-field.js
+++ app/components/listing-field/listing-field.js
@@ -7,17 +7,6 @@ angular.module('carnival.components.listingfield', [])
       item: '=',
       field: '='
     },
-    templateUrl: 'components/listing-field/listing-field.html',
-    controller: function($scope, $stateParams, Configuration){
-      var entityModel = Configuration.getEntity($stateParams.entity);
-        
-      $scope.getHtml = function(){
-        if($scope.field.type != 'belongsTo' && $scope.field.type != 'hasMany'){
-          return $scope.item[$scope.field.name]
-        }else if($scope.field.type != 'belongsTo'){
-           
-        }
-      }
-    }
+    templateUrl: 'components/listing-field/listing-field.html'    
   };
 });
