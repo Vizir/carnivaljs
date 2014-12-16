0308ae856ee2f9fbfb8a3795db84ef78d8adc961
Joao Kiyoshi <joao@vizir.com>
2014-12-16 17:31:16 -0200

    Notification test

---
+++ app/services/notification/notification.spec.js
@@ -0,0 +1,29 @@
+describe('On notification service', function () {
+
+  var scope, Notification, notificationFactory, $timeout;
+
+  beforeEach(function () {
+
+    module('carnival');
+    inject(function ($rootScope, _Notification_, _notificationFactory_, _$timeout_) {
+      scope = $rootScope.$new();
+      Notification = _Notification_;
+      notificationFactory = _notificationFactory_;
+      $timeout = _$timeout_;
+    });
+
+  });
+
+  it('should add notifications to factory', function () {
+    new Notification('Mario', 'error');
+    new Notification('Luigi', 'success');
+    expect(notificationFactory.length).to.be.equal(2);
+  });
+
+  it('should remove notifications after its timeout', function () {
+    new Notification('Mario', 'error');
+    $timeout.flush();
+    expect(notificationFactory.length).to.be.equal(0);
+  });
+
+});
