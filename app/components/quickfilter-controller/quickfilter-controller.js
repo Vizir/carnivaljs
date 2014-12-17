6eab7706634a27a54687979fa3dd41c5dfd2d35a
Joao Kiyoshi <joao@vizir.com>
2014-12-17 18:39:58 -0200

    Add a disabled state

--- app/components/quickfilter-controller/quickfilter-controller.js
+++ app/components/quickfilter-controller/quickfilter-controller.js
@@ -11,6 +11,11 @@ angular.module('carnival.components.quickfilter-controller', [])
 
       var searchs = urlParams.getFilter('search') || {};
 
+      scope.isSelected = function (field, value) {
+        if (!searchs[field]) return false;
+        return value === searchs[field];
+      };
+
       scope.setFilter = function (field, value) {
         searchs[field] = value;
         urlParams.setFilter('search', searchs, true);
