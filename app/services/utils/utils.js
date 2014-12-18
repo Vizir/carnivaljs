fff492c32049ad7fea86085ad7a24a78343f3cd6
Joao Kiyoshi <joao@vizir.com>
2014-12-18 14:39:02 -0200

    Fix on utils.cutString

--- app/services/utils/utils.js
+++ app/services/utils/utils.js
@@ -1,18 +1,12 @@
 angular.module('carnival')
 .service('utils', function () {
 
-  var cutString = function (str, n, etc) {
+  var cutString = function (str, n) {
     if (!n) return str;
-    if (!str) return new Error('cutString: to cut a string you must pass a string!');
+    if (!str) throw 'cutString: to cut a string you must pass a string!';
     if (str.length <= n) return str;
-
-    var result = '';
-    for (var i = 0; i < n; i += 1) {
-      result += str[i];
-    }
-
-    if (etc) result += '...';
-    return result;
+    str = str.substr(0, n - 1) + '...';
+    return str;
   };
 
   return {
