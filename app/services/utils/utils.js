2cdfa7a918e44a30e80f3ebe608a05c50c3340ce
diegonakamashi <diegonakamashi@gmail.com>
2015-01-21 15:41:53 -0200

    fix cutstring method when parameter == number

--- app/services/utils/utils.js
+++ app/services/utils/utils.js
@@ -5,6 +5,7 @@ angular.module('carnival')
     if (!n) return str;
     if (!str) throw 'cutString: to cut a string you must pass a string!';
     if (str.length <= n) return str;
+    if (typeof str !== 'string') return str;
     str = str.substr(0, n - 1) + '...';
     return str;
   };
