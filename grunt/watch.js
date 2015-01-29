883d21dcdd7114f214ec383b0c95b4c2117e8362
Joao Kiyoshi <joao@vizir.com.br>
2015-01-29 15:35:11 -0200

    Add clean task after watch

--- grunt/watch.js
+++ grunt/watch.js
@@ -1,7 +1,7 @@
 module.exports = {
   dev: {
     files: ['Gruntfile.js', '!src/**/*.spec.js', 'src/**/*.js', 'src/**/*.html'],
-    tasks: ['html2js:dist', 'concat:dist', 'ngAnnotate:dist']
+    tasks: ['html2js:dist', 'concat:dist', 'ngAnnotate:dist', 'clean:tmp']
   },
   options: {
     atBegin: true
