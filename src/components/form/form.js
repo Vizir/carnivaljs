e63fac3283cdc212cfd40cbdaf7ebedb08b72d68
diegonakamashi <diegonakamashi@gmail.com>
2015-02-22 17:01:47 -0300

    fix column form bug

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -71,6 +71,9 @@ angular.module('carnival.components.form', [])
         new Notification(message, 'success');
         if($scope.type === 'normal')
           $state.go('main.edit', { entity: $scope.entity.name, id: data.id});
+        else
+          $scope.state = 'edit';
+
         $document.scrollTop(window.innerHeight, 1000);
       };
 
