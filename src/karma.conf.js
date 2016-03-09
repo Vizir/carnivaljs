69caee851f3fbfe73390a0adfc6d7ca544ffd672
Tiago de Assis Gonçalves <contato@tiagogoncalves.com.br>
2016-03-09 01:04:26 -0300

    fixed tests.

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -9,7 +9,7 @@ module.exports = function(config) {
       'bower_components/ng-wig/dist/ng-wig.js',
       'bower_components/angular-loading-bar/build/loading-bar.js',
       'bower_components/angular-scroll/angular-scroll.js',
-      'bower_components/angular-datepicker/dist/index.js',
+      'bower_components/angular-datepicker/dist/angular-datepicker.js',
       'bower_components/string-mask/src/string-mask.js',
       'bower_components/sweetalert/lib/sweet-alert.js',
       'build/tmp/*.js',
